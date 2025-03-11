d1a237d4f87cc6a8273c2838d8e53a791d462e5e
Giovanni Peirone <134148500+GiovanniPeirone@users.noreply.github.com>
2025-03-11 13:02:24 -0300

    listo

---
+++ descuentos/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Ingrese el precio del producto: ");
+float precio = float.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese el Descuento: ");
+float descuento = float.Parse(Console.ReadLine());
+precio = precio * (1 - descuento / 100);
+Console.WriteLine("El precio final es: " + precio);
---
+++ diassemanas/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Ingrese el número de días: ");
+int dias = int.Parse(Console.ReadLine());
+int semanas = dias / 7;
+int diasRestantes = dias % 7;
+Console.WriteLine($"{dias} días equivalen a {semanas} semanas y {diasRestantes} días.");
