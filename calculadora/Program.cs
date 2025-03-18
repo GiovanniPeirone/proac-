e879bfac2794ebd1b691a415add592e3ed22c1b4
Giovanni Peirone <134148500+GiovanniPeirone@users.noreply.github.com>
2025-03-18 14:14:14 -0300

    mas

---
+++ calculadora/Program.cs
@@ -0,0 +1,53 @@
+﻿
+while (true)
+{
+    Console.WriteLine("Calculadora");
+    Console.WriteLine("1. Sumar");
+    Console.WriteLine("2. Restar");
+    Console.WriteLine("3. Multiplicar");
+    Console.WriteLine("4. Dividir");
+    Console.WriteLine("5. Salir");
+    Console.Write(">");
+
+    int opcion = int.Parse(Console.ReadLine());
+    switch (opcion)
+    {
+        case 1:
+            Console.Write("Ingrese el primer número: ");
+            double num1 = double.Parse(Console.ReadLine());
+            Console.Write("Ingrese el segundo número: ");
+            double num2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Resultado: " + (num1 + num2));
+            break;
+        case 2:
+            Console.Write("Ingrese el primer número: ");
+            num1 = double.Parse(Console.ReadLine());
+            Console.Write("Ingrese el segundo número: ");
+            num2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Resultado: " + (num1 - num2));
+            break;
+        case 3:
+            Console.Write("Ingrese el primer número: ");
+            num1 = double.Parse(Console.ReadLine());
+            Console.Write("Ingrese el segundo número: ");
+            num2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Resultado: " + (num1 * num2));
+            break;
+        case 4:
+            Console.Write("Ingrese el primer número: ");
+            num1 = double.Parse(Console.ReadLine());
+            Console.Write("Ingrese el segundo número: ");
+            num2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Resultado: " + (num1 / num2));
+            break;
+        case 5:
+            Console.WriteLine("Saliendo...");
+            break;
+    }
+    if (opcion == 5)
+    {
+        break;
+    }
+}
+
+
---
+++ menudeopciones/Program.cs
@@ -0,0 +1,46 @@
+﻿
+
+using System.Xml.Serialization;
+
+string[] opciones = {
+    "Opcion 1 : directorio",
+    "Opcion 2 : time",
+    "Opcion 3 : date",
+    "Opcion 4 : exit"
+};
+
+
+while (true)
+{
+    MostrarMenu(opciones);
+    Console.Write(">");
+    
+    int opcion = int.Parse(Console.ReadLine());
+    switch (opcion)
+    {
+        case 1:
+            Console.WriteLine("Directorio actual: " + Directory.GetCurrentDirectory());
+            break;
+        case 2:
+            Console.WriteLine("Hora actual: " + DateTime.Now.ToString("HH:mm:ss"));
+            break;
+        case 3:
+            Console.WriteLine("Fecha actual: " + DateTime.Now.ToString("dd/MM/yyyy"));
+            break;
+        case 4:
+            Console.WriteLine("Saliendo...");
+            break;
+    }       
+    if (opcion == 4)
+    {
+            break;
+    }
+}
+
+void MostrarMenu(string[] opciones)
+{
+    for (int i = 0; i < opciones.Length; i++)
+    {
+        Console.WriteLine(opciones[i]);
+    }
+}
