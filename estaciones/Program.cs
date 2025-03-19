ccac36ad79e4eba7ad445a433f89a2b37533c8c9
Giovanni Peirone <134148500+GiovanniPeirone@users.noreply.github.com>
2025-03-19 13:42:10 -0300

    2 mas

---
+++ estaciones/Program.cs
@@ -0,0 +1,23 @@
+﻿
+Console.Write("Estaciones del año \n 1 - Verano \n 2 - Otoño \n 3 - Invierno \n 4 -  Primavera \n");
+int opcion = Convert.ToInt32(Console.ReadLine());
+
+switch(opcion){
+    case 1:
+        Console.WriteLine("Verano");
+        Console.WriteLine("Del 21 de diciembre a 20 de marzo");
+        break;
+    case 2:
+        Console.WriteLine("Otoño");
+        Console.WriteLine("Del 21 de marzo a 20 de junio");
+        break;
+    case 3:
+        Console.WriteLine("Del 21 de junio a 20 de septiembre");
+        break;
+    case 4:
+        Console.WriteLine("Del 21 de septiembre a 20 de diciembre");
+        break;
+    default:
+        Console.WriteLine("Opción no válida");
+        break;
+}
---
+++ vocal/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+Console.Write("Escriba una vocal: ");
+char vocal = Console.ReadLine()[0];
+
+switch (vocal)
+{
+    case 'a':
+        Console.WriteLine("avión ");
+        break;
+    case 'e':
+        Console.WriteLine("elefante ");
+        break;
+    case 'i':
+        Console.WriteLine("iglesia ");
+        break;
+    case 'o':
+        Console.WriteLine("oso ");
+        break;
+    case 'u':
+        Console.WriteLine("uva ");
+        break;
+    default:
+        Console.WriteLine("No es una vocal");
+        break;
+}
+
+
+
